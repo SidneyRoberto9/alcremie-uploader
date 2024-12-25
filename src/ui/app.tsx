e3b566d421ba858592faddda5772028a1010e15a
Sidney Roberto <sidneyrpsilva@gmail.om>
2024-12-25 11:51:33 -0300

    first commit

---
+++ src/ui/app.tsx
@@ -0,0 +1,9 @@
+export const App = () => {
+  return (
+    <div className='m-auto h-screen bg-gray-800'>
+         <h1 className='text-2xl text-white'>Vite + React</h1>
+    </div>
+  )
+}
+
+ 
---
+++ src/ui/main.tsx
@@ -0,0 +1,14 @@
+import '@/ui/styles/globals.css';
+
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+
+import { App } from '@/ui/app';
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
